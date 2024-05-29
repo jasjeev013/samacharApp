211ee3354c109b3dcfecb1f9e9e0a502ab39be30
Jasjeev <jasjeev99@gmail.com>
2024-05-30 02:26:24 +0530

    Added Dark Mode & Fetching Data from API Done

--- src/App.js
+++ src/App.js
@@ -4,11 +4,31 @@ import Navbar from './components/Navbar';
 import News from './components/News';
 
 export default class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      mode: 'light'
+    }
+  }
+
+  
+
+
   render() {
+
     return (
       <div>
-        <Navbar/>
-        <News/>
+        <Navbar mode = {this.state.mode} setMode = {() =>{
+          if(this.state.mode === 'light'){
+            this.setState({mode:'dark'});
+            document.body.style.backgroundColor = '#743232';
+          }else{
+            this.setState({mode:'light'});
+            document.body.style.backgroundColor = '#B89DE4';
+          }
+        }} />
+        <News mode = {this.state.mode} />
       </div>
     )
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
 
+
 export default class Navbar extends Component {
+
+
     
 
     render() {
+
+        let { mode, setMode } = this.props;
         return (
             <div>
-                <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="/">SamacharApp</a>
+                <nav className={`navbar navbar-expand-lg navbar-${mode}`} style={{
+                    backgroundColor: mode === 'light' ? '#C9E49D' : '#406E6E',
+                
+                }}>
+                    <div className="container-fluid ">
+                        <a className="navbar-brand" href="/"> <b>SamacharApp</b> </a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
@@ -21,7 +29,9 @@ export default class Navbar extends Component {
                                     <a className="nav-link" href="/">About</a>
                                 </li>
                             </ul>
-
+                            <div className="form-check form-switch">
+                                <input className="form-check-input" onClick={setMode} type="checkbox"  role="switch" id="flexSwitchCheckDefault" />
+                            </div>
                         </div>
                     </div>
                 </nav>
